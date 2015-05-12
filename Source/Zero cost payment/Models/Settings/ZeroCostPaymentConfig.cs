cc264b6c0de91d567ffa6981061fd1b0c0ada66f
Brian Lyk Larsen <brianlarsen1991@outlook.com>
2015-05-12 10:23:19 +0200

    Initial commit

---
+++ Source/Zero cost payment/Models/Settings/ZeroCostPaymentConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Zero_cost_payment_method_management.Models.Settings {
+  public class ZeroCostPaymentConfig {
+    [XmlElement( "zeroCostPayment" )]
+    public List<ZeroCostPaymentSetting> Settings = new List<ZeroCostPaymentSetting>();
+  }
+
+  public class ZeroCostPaymentSetting {
+    [XmlAttribute( "storeId" )]
+    public long StoreId { get; set; }
+
+    [XmlAttribute( "methodId" )]
+    public long PaymentMethodId { get; set; }
+  }
+}
